85b73480d28a02c243961335163a69b6419a7e97
Appi72020 <abdoulraouf72020@gmail.com>
2023-01-27 01:32:20 +0100

    finish main menu

--- Virtuel-World/Assets/_Scripts/BackGround.cs
+++ Virtuel-World/Assets/_Scripts/BackGround.cs
@@ -1,67 +1,44 @@
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class BackGround : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public Image defaultImage; // l'image par défaut 
-    public Button[] buttons; // les boutons à surveiller
-    public Image[] hoverImages; // les images de survol correspondantes à chaque bouton
-
-    private void Start()
-    {
-        // Affiche l'image par défaut au début
-        //defaultImage.gameObject.SetActive(true);
-        foreach (var hover in hoverImages)
-        {
-            hover.gameObject.SetActive(false);
-        }
-        // Ajoute un écouteur d'événement pour chaque bouton pour détecter le survol
-        for (int i = 0; i < buttons.Length; i++)
-        {
-            int index = i;
-            buttons[i].onClick.AddListener(() => OnButtonHover(index));
-        }
-    }
-
-    private void OnButtonHover(int index)
-    {
-        // Affiche l'image de survol correspondante lorsque le bouton est survolé
-        //defaultImage.gameObject.SetActive(false);
-        for (int i = 0; i < hoverImages.Length; i++)
-        {
-            if (i == index)
-            {
-                hoverImages[i].gameObject.SetActive(true);
-            }
-            else
-            {
-                hoverImages[i].gameObject.SetActive(false);
-            }
-        }
-    }
+    /*[SerializeField] private List<Button> buttonList= new List<Button>(6);
+    [SerializeField] private List<Image> imageList= new List<Image>(6);*/
+    [SerializeField] public Image defaultImage;
+    [SerializeField] public Image backGroundImage;
+    [SerializeField] private bool isHovering = false;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        // Affiche l'image de survol correspondante lorsque le bouton est survolé
+        isHovering = true;
+        backGroundImage.gameObject.SetActive(true);
         defaultImage.gameObject.SetActive(false);
-        for (int i = 0; i < buttons.Length; i++)
-        {
-            if (eventData.pointerEnter == buttons[i].gameObject)
-            {
-                hoverImages[i].gameObject.SetActive(true);
-                break;
-            }
-        }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        // Affiche l'image par défaut lorsque le bouton n'est plus survolé
+        isHovering = false;
+        backGroundImage.gameObject.SetActive(false);
         defaultImage.gameObject.SetActive(true);
-        foreach (var hover in hoverImages)
+    }
+
+    private void Start()
+    {
+        defaultImage.gameObject.SetActive(true);
+        
+    }
+    void Update()
+    {
+        if (isHovering)
         {
-            hover.gameObject.SetActive(false);
+            Debug.Log("over button");
+        }
+        else
+        {
+            Debug.Log("none button");
         }
     }
 }
